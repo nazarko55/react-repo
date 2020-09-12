5e25da61e4987804cf63687043b5ff4a5e5a5447
Nazar <sikirinskijn@gmailcom>
2020-09-12 15:58:55 +0300

    jsx react app

---
+++ Lesson1/task6/review_build/precache-manifest.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab513eade59116900c9bed6babf3bff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ef5ed4e6f6b38a1f9b",
+    "url": "/static/css/main.83078075.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ef5ed4e6f6b38a1f9b",
+    "url": "/static/js/main.7e3a42c0.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);

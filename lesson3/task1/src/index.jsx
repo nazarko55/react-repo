0defff71dff9ade8108e23204f49a6f270ed2eb4
Nazar <sikirinskijn@gmailcom>
2020-09-15 12:38:26 +0300

    render functional component

---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import Search from './Search.jsx';
+
+ReactDOM.render
+  (<Search name="Bob" />,
+    document.getElementById('root')
+  );
+

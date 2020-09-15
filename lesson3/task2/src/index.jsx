ec7777b1db3dacd39104b30e236a8355ffae2c5d
Nazar <sikirinskijn@gmailcom>
2020-09-15 13:01:17 +0300

    render class component

---
+++ lesson3/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import SearchField from './Search.jsx';
+
+ReactDOM.render
+  (<SearchField name="Bob" />, document.getElementById('root'));

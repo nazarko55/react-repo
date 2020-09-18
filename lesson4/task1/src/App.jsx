8b189e10dce4fe39240e2e68caaba842623ab3a4
Nazar <sikirinskijn@gmailcom>
2020-09-18 15:44:58 +0300

    render counter, usage Setstate

---
+++ lesson4/task1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './Counter.jsx';
+
+
+const App = () => {
+  return (
+    <>
+      <Counter start={0} interval={500} />
+      <Counter start={17} interval={1000} />
+      <Counter start={-50} interval={100} />
+    </>
+  );
+};
+
+export default App;

d795b3937a38be7d3bfb6f7bedeaf05ae96eeaf1
Nazar <sikirinskijn@gmailcom>
2020-09-25 19:36:00 +0300

    render transactions list

---
+++ lesson7/task3/src/TransactionsList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Transaction from './Transaction';
+
+const TransactionsList = ({ transactions }) => {
+  return (
+    <ul className="transactions">
+      {transactions.map(transaction =>
+        <li key={transaction.id} className="transaction">
+          <Transaction {...transaction} />
+        </li>)}
+    </ul>
+
+  )
+}
+
+export default TransactionsList;

7348cb57423f07adc757d4593e4729ea112eab5e
Nazar <sikirinskijn@gmailcom>
2020-09-28 18:40:11 +0300

    lifecycle methods

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
+import React from "react";
 
 class Life extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     console.log('constructor: good place to create state');
   }
+
   componentDidMount() {
-    console.log(' componentDidMount: API calls, subscriptions');
+    console.log('componentDidMount: API calls, subscriptions');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render ');
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
     return true;
   }
+
   componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new prop');
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
     console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
   }
+
   render() {
     console.log('return React element to build DOM');
-
-    return <div className="number">{this.props.number}</div>;
+    return <div className="number">{this.props.number}</div>
   }
 }
 

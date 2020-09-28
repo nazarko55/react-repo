f5c7febc14f39ef3b05057ccd8790b87ba6a3f75
Nazar <sikirinskijn@gmailcom>
2020-09-28 23:44:29 +0300

    componentWillUnmount

---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class Dimensions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: null,
+      height: null,
+    }
+  }
+
+  componentDidMount() {
+    const { innerHeight, innerWidth } = window;
+
+    this.setDimensions(innerWidth, innerHeight)
+    window.addEventListener('resize', this.onResize);
+  }
+
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+
+  onResize = e => {
+    const { innerWidth, innerHeight } = window;
+
+    this.setDimensions(innerHeight, innerWidth)
+
+  }
+
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${innerWidth} x ${innerHeight}`;
+  }
+
+  render() {
+    const { width, height } = this.state;
+
+    return <div className="dimensions">
+      {`${width}px - ${height}px`}
+    </div>
+  }
+}
+
+export default Dimensions;

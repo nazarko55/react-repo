b6dfc0ecca91fa14a71a1497b5bc1617c1149f06
Nazar <sikirinskijn@gmailcom>
2020-10-10 14:13:27 +0300

    Expanded Component

---
+++ Lesson11/task3/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Expand from './Expand';
+
+class Page extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggleDialog = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    const element = <p>
+      Hooks are a new addition in React 16.8. They let you use state and
+      other React features without writing a class.
+          </p>
+    return (
+      <div className="app">
+        <Expand
+          title="Some text"
+          isOpen={this.state.isOpen}
+          onClose={this.toggleDialog}
+        >
+          {element}
+        </Expand>
+      </div>
+    )
+  }
+};
+
+export default Page;

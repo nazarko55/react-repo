dc3035b8b84ccb34369d9d8c553e427d42edc19e
Nazar <sikirinskijn@gmailcom>
2020-10-14 14:01:54 +0300

    react router -

---
+++ lesson13/task1/src/Contacts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Contacts() {
+  return (
+    <div className="page_content">
+      <h1>Contacts</h1>
+      <p>We are here</p>
+      <Link to="">Go Home</Link>
+    </div>
+  );
+}
+
+export default Contacts;

67d639170a786e250a45c0914522c7e14cbf4543
Nazar <sikirinskijn@gmailcom>
2020-10-18 14:08:48 +0300

    React Hooks

---
+++ lesson14/task4/src/ConnectionStatus.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const ConnectionStatus = () => {
+  const [status, setStatus] = useState('online');
+
+  useEffect(() => {
+    window.addEventListener('online', () => setStatus('online'));
+    window.addEventListener('offline', () => setStatus('offline'));
+    return () => {
+      window.removeEventListener('online', () => setStatus('online'));
+      window.removeEventListener('offline', () => setStatus('offline'));
+    }
+  }, []);
+
+  const statusChange = status === 'offline'
+    ? 'status_offline'
+    : ''
+  return <div className={`status ${statusChange}`}>{status}</div>
+};
+
+export default ConnectionStatus;
---
+++ lesson14/task4/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import ConnectionStatus from './ConnectionStatus.jsx';
+
+ReactDOM.render(
+  <ConnectionStatus />,
+  document.getElementById('root')
+);
